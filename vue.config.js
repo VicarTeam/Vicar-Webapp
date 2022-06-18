bc8dd50c32585d4b4f905fa23be1ee27e99df960
DasDarki <rageskilln@gmail.com>
2022-06-18 18:11:48 +0200

    adding tauri

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: ''
+  publicPath: '',
+  outputDir: 'src-tauri/target/webpack_dist'
 })
