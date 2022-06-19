d97aa6dd8b0fb119d19480a8a4d5d43532ddc7e6
DasDarki <rageskilln@gmail.com>
2022-06-19 20:04:12 +0200

    adding share service VicarShare

---
+++ src/components/main/characters/Character.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="character-entry">
+    <Avatar :src="character.avatar" style="width: 5rem; height: 5rem"/>
+
+    <div class="info">
+      <span class="title">{{character.name}}</span>
+      <span class="subtitle">
+            {{$t('character.sex.' + character.sex)}}
+            <bullet/>
+            {{character.concept}}
+            <bullet v-if="character.concept"/>
+            <i> Clan:</i> {{character.clan.name}}
+            <bullet/>
+            {{character.clan.slogan}}
+            <bullet/>
+            <i> Generation:</i> {{character.generation}} ({{$t('character.generation.' + character.generationEra)}})
+            <bullet v-if="character.chronicle"/>
+            {{character.chronicle}}
+            <bullet v-if="character.exp > 0"/>
+            <span v-if="character.exp > 0"><i> EXP:</i> {{character.exp}}</span>
+          </span>
+    </div>
+
+    <div class="actions">
+      <IconButton icon="fa-trash" @click="beginCharDeletion(character)"/>
+      <IconButton icon="fa-copy" @click="cloneCharacter(character)"/>
+      <IconButton icon="fa-file-arrow-down" @click="exportCharacter(character)"/>
+      <IconButton v-if="isShareAvailable()" icon="fa-share-nodes" @click="shareCharacter(character)"/>
+      <IconButton icon="fa-eye" @click="viewCharacter(character)"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Inject, Prop, Vue} from "vue-property-decorator";
+import Bullet from "@/components/Bullet.vue";
+import Avatar from "@/components/Avatar.vue";
+import IconButton from "@/components/IconButton.vue";
+import {ICharacter} from "@/types/models";
+import CharacterStorage from "@/libs/io/character-storage";
+import FileCreator from "@/libs/io/file-creator";
+import {Mutation} from "vuex-class";
+
+@Component({
+  components: {IconButton, Avatar, Bullet}
+})
+export default class Character extends Vue {
+
+  @Prop({required: true})
+  private character!: ICharacter;
+
+  @Mutation("setEditingCharacter")
+  private setEditingCharacter!: (character: ICharacter) => void;
+
+  @Mutation("setLevelMode")
+  private setLevelMode!: (mode: boolean) => void;
+
+  private cloneCharacter(character: ICharacter) {
+    const newChar = {...character};
+    newChar.name += " - " + this.$t('character.copy').toString();
+    CharacterStorage.addCharacter(newChar);
+    this.updateCharacterList();
+  }
+
+  private exportCharacter(char: ICharacter) {
+    FileCreator.create(char.name + ".json", JSON.stringify(char));
+  }
+
+  private viewCharacter(character: ICharacter) {
+    this.setLevelMode(false);
+    this.setEditingCharacter(character);
+    this.$router.push({name: 'viewer'});
+  }
+
+  @Inject("begin-char-deletion")
+  private beginCharDeletion!: (character: ICharacter) => void;
+
+  @Inject("update-character-list")
+  private updateCharacterList!: () => void;
+
+  @Inject("is-share-available")
+  private isShareAvailable!: () => boolean;
+
+  @Inject("share-character")
+  private shareCharacter!: (character: ICharacter) => void;
+}
+</script>
+
+<style scoped lang="scss">
+.character-entry {
+  user-select: none;
+  padding: 1rem 1.5rem;
+  border-radius: 8px;
+  border: 1px solid var(--primary-color);
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  .info {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    .title {
+      text-transform: uppercase;
+      font-size: 1.5rem;
+      font-weight: bolder;
+      color: #fff;
+    }
+    .subtitle {
+      font-size: 1.1rem;
+      color: #939393;
+    }
+  }
+  .actions {
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
--- src/components/main/characters/Characters.vue
+++ src/components/main/characters/Characters.vue
@@ -8,38 +8,12 @@
       <button class="btn btn-primary" style="height: 4rem" @click="importFiles.click()">
         {{$t('main.characters.import')}}
       </button>
+
+      <VicarShare ref="vicarShare"/>
     </div>
 
     <div class="d-flex flex-column flex-grow-1" style="gap: 2rem">
-      <div class="character-entry" v-for="c in getCharacters()">
-        <Avatar :src="c.avatar" style="width: 5rem; height: 5rem"/>
-
-        <div class="info">
-          <span class="title">{{c.name}}</span>
-          <span class="subtitle">
-            {{$t('character.sex.' + c.sex)}}
-            <bullet/>
-            {{c.concept}}
-            <bullet v-if="c.concept"/>
-            <i> Clan:</i> {{c.clan.name}}
-            <bullet/>
-            {{c.clan.slogan}}
-            <bullet/>
-            <i> Generation:</i> {{c.generation}} ({{$t('character.generation.' + c.generationEra)}})
-            <bullet v-if="c.chronicle"/>
-            {{c.chronicle}}
-            <bullet v-if="c.exp > 0"/>
-            <span v-if="c.exp > 0"><i> EXP:</i> {{c.exp}}</span>
-          </span>
-        </div>
-
-        <div class="actions">
-          <IconButton icon="fa-trash" @click="confirmCharDeletionModal.showModal(c)"/>
-          <IconButton icon="fa-copy" @click="cloneCharacter(c)"/>
-          <IconButton icon="fa-file-arrow-down" @click="exportCharacter(c)"/>
-          <IconButton icon="fa-eye" @click="viewCharacter(c)"/>
-        </div>
-      </div>
+      <Character v-for="c in getCharacters()" :character="c"/>
     </div>
 
     <CreateCharacterModal ref="createCharacterModal"/>
@@ -48,21 +22,23 @@
 </template>
 
 <script lang="ts">
-import {Component, Ref, Vue} from "vue-property-decorator";
+import {Component, Provide, Ref, Vue} from "vue-property-decorator";
 import Modal from "@/components/modal/Modal.vue";
-import CreateCharacterModal from "@/components/main/characters/CreateCharacterModal.vue";
+import CreateCharacterModal from "@/components/main/characters/modals/CreateCharacterModal.vue";
 import Avatar from "@/components/Avatar.vue";
 import {ICharacter} from "@/types/models";
 import CharacterStorage from "@/libs/io/character-storage";
-import ConfirmCharDeletionModal from "@/components/main/characters/ConfirmCharDeletionModal.vue";
+import ConfirmCharDeletionModal from "@/components/main/characters/modals/ConfirmCharDeletionModal.vue";
 import FileCreator from "@/libs/io/file-creator";
 import FileReaderUtils from "@/libs/io/file-reader";
 import {Mutation} from "vuex-class";
 import Bullet from "@/components/Bullet.vue";
 import IconButton from "@/components/IconButton.vue";
+import VicarShare from "@/components/main/characters/share/VicarShare.vue";
+import Character from "@/components/main/characters/Character.vue";
 
 @Component({
-  components: {IconButton, Bullet, ConfirmCharDeletionModal, Avatar, CreateCharacterModal, Modal}
+  components: {Character, VicarShare, IconButton, Bullet, ConfirmCharDeletionModal, Avatar, CreateCharacterModal, Modal}
 })
 export default class Characters extends Vue {
 
@@ -75,11 +51,8 @@ export default class Characters extends Vue {
   @Ref("confirmCharDeletionModal")
   private confirmCharDeletionModal!: ConfirmCharDeletionModal;
 
-  @Mutation("setEditingCharacter")
-  private setEditingCharacter!: (character: ICharacter) => void;
-
-  @Mutation("setLevelMode")
-  private setLevelMode!: (mode: boolean) => void;
+  @Ref("vicarShare")
+  private vicarShare!: VicarShare;
 
   private async importCharacterFromFile(event: {target: {files: FileList}}) {
     try {
@@ -91,62 +64,36 @@ export default class Characters extends Vue {
     }
   }
 
-  private cloneCharacter(character: ICharacter) {
-    const newChar = {...character};
-    newChar.name += " - Kopie";
-    CharacterStorage.addCharacter(newChar);
-    this.$forceUpdate();
-  }
-
   private onCharDeleted() {
     this.$forceUpdate();
-  }
-
-  private exportCharacter(char: ICharacter) {
-    FileCreator.create(char.name + ".json", JSON.stringify(char));
-  }
-
-  private viewCharacter(character: ICharacter) {
-    this.setLevelMode(false);
-    this.setEditingCharacter(character);
-    this.$router.push({name: 'viewer'});
   }
 
   private getCharacters(): ICharacter[] {
     return CharacterStorage.loadedCharacters;
   }
+
+  @Provide("begin-char-deletion")
+  private beginCharDeletion(char: ICharacter) {
+    this.confirmCharDeletionModal.showModal(char);
+  }
+
+  @Provide("update-character-list")
+  private updateCharacterList() {
+    this.$forceUpdate();
+  }
+
+  @Provide("is-share-available")
+  private isShareAvailable(): boolean {
+    return this.vicarShare.isAvailable;
+  }
+
+  @Provide("share-character")
+  private shareCharacter(char: ICharacter) {
+    this.vicarShare.shareCharacter(char);
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.character-entry {
-  user-select: none;
-  padding: 1rem 1.5rem;
-  border-radius: 8px;
-  border: 1px solid var(--primary-color);
-  display: flex;
-  flex-direction: row;
-  gap: 1rem;
-  .info {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    .title {
-      text-transform: uppercase;
-      font-size: 1.5rem;
-      font-weight: bolder;
-      color: #fff;
-    }
-    .subtitle {
-      font-size: 1.1rem;
-      color: #939393;
-    }
-  }
-  .actions {
-    display: flex;
-    gap: 1rem;
-    justify-content: center;
-    align-items: center;
-  }
-}
+
 </style>
